156120efd25e9d756f883e6e44c3a242bdf1d370
Vladislav Ulandaev <55333013+Ulandaev@users.noreply.github.com>
2021-05-21 13:21:36 +0300

    Update server.py

--- Checkers_server/server.py
+++ Checkers_server/server.py
@@ -1,8 +1,11 @@
 from my_socket import My_socket
-import threading
+import threading, sys, socket
 
 
 class Server(My_socket):
+
+    k = 0
+    stop = True
     def __init__(self):
         super(Server, self).__init__()
 
@@ -11,9 +14,13 @@ class Server(My_socket):
         self.users = []
 
     def set_up(self):
-        server.bind(("192.168.43.155", 54312))
+        #192.168.43.155
+        self.bind(("127.0.0.1", 54325))
         self.listen(7)
         self.accept_sockets()
+        self.shutdown(socket.SHUT_RDWR)
+        self.close()
+        
 
     def send_data(self, data, user):
         user.send(data)
@@ -21,16 +28,42 @@ class Server(My_socket):
     def listen_socket(self, listened_socket, user):
         print("Listening user")
 
-        while True:
+
+        while self.stop:
             data = listened_socket.recv(2048)
             print(f"user sent {data}")
             if data == 0:
                 break
 
             self.send_data(data, user)
+            if user == self.users[1]:
+                self.k += 1
+            elif user == self.users[0]:
+                self.k -= 1
+            print(self.k)
+
+            if self.k > 3 or self.k < -1:
+
+                # self.send_data(self.data_error, self.users[0])
+                # self.send_data(self.data_error, self.users[1])
+
+                self.stop = False
+                if user == self.users[0]:
+                    self.data_error = "0000E"
+                    self.data_error = self.data_error.encode("utf-8")
+                    self.users[0].send(self.data_error)
+                    # self.users[1].send(self.data_error)
+                elif user == self.users[1]:
+                    self.data_error = "0000E"
+                    self.data_error = self.data_error.encode("utf-8")
+                    # self.users[0].send(self.data_error)
+                    self.users[1].send(self.data_error)
+                # self.close()
+
 
     def accept_sockets(self):
         self.step = 0
+
         while True:
             user_socket, adress = self.accept()
             print(f"user <{adress[0]}> connected")
@@ -43,81 +76,28 @@ class Server(My_socket):
                     user.send(f"{self.step}R".encode("utf-8"))
                     self.step += 1
 
+                print(self.users[0])
+                print(self.users[1])
+
+
                 listened_accepted_user_1 = threading.Thread(target=self.listen_socket, args=(self.users[0],self.users[1]))
                 listened_accepted_user_1.start()
 
+
                 listened_accepted_user_2 = threading.Thread(target=self.listen_socket, args=(self.users[1],self.users[0]))
                 listened_accepted_user_2.start()
+                listened_accepted_user_1.join()
+                listened_accepted_user_2.join()
+                print("close s")
+                break
+                # server.close()
 
 
 if __name__ == "__main__":
     server = Server()
     server.set_up()
+    # server.close()
+    print("success")
+    sys.exit()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
-# import sys
-# import socket
-# import threading
-# import time
-#
-# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# server.bind(("127.0.0.1", 1234))
-#
-# server.listen(4)send
-# print("server is listening")
-#
-# users = []
-# def send_all(data):
-#     for user in users:
-#          user.send(data)
-#
-# def listen_user(user):
-#     print("listening user")
-#     while True:
-#         data = user.recv(2048)
-#         print(f"User sent {data}")
-#         send_all(data)
-#
-# def start_server():
-#     while True:
-#
-#         user_socket , adress = server.accept()
-#         print(f"user {adress[0]} connected!")
-#         users.append(user_socket)
-#         listen_accepted_user = threading.Thread(target=listen_user, args=(user_socket,))
-#         listen_accepted_user.start()
-#         a = input()
-#         print(a)
-#
-#
-#
-# if __name__ == '__main__':
-#     start_server()
-#
-#
-#
-#
-#
-#
